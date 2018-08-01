e461e4651453ad36576ebee079f5fb19f80cb441
15EC048 <brindhasss.mk6@gmail.com>
2018-08-01 16:14:19 +0530

    Create 1f.java

---
+++ player/1f.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class TestIsomorphic
+{
+      public static void main(String[] args) 
+      {
+            String str1 = "abb";
+            String str2 = "axx";
+            boolean res = TestIsomorphic.checkIsomorphic(str1,str2);
+            System.out.println(res);
+      }
+}
